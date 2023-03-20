efbe9fb90e377bafa2c240b2dc3b0de3a5dfb0f8
Vitória <vitoriapiloto477@gmail.com>
2023-03-20 15:42:34 -0300

    renderização de componentes condicional a cada clique no indice

--- porfolio/src/App.jsx
+++ porfolio/src/App.jsx
@@ -17,20 +17,48 @@ function App() {
         {...item}
       />
     )
+
+    const [academia, setAcademia] = React.useState(false)
+    const [experiencia, setExperiencia] = React.useState(false)
+    const [curiosidade, setCuriosidade] = React.useState(false)
+    const [projeto, setProjeto] = React.useState(false)
+    const [resumo, setResumo] = React.useState(false)
+
+    function mostrarResumo(){
+      setResumo(oldResumo => !oldResumo)
+    }
+    function mostrarAcademico(){
+      setAcademia(oldAcademia => !oldAcademia)
+    }
+    function mostrarExperiencia(){
+      setExperiencia(oldExperiencia => !oldExperiencia)
+    }
+    function mostrarCuriosidade(){
+      setCuriosidade(oldCuriosidade => !oldCuriosidade)
+    }
+    function mostrarProjeto(){
+      setProjeto(oldProjeto => !oldProjeto)
+    }
   return (
     <>
       <BarraLateral/>
-      <Indice/>
-      <h1 id='titulo'>Resumo</h1>
-      <Resumo/>
-      <h1 id='titulo'>Formação Acadêmica</h1>
-      <Academico/>
-      <h1 id='titulo'>Experiência Anterior</h1>
-      <Experiencias/>
-      <h1 id='titulo'>Curiosidades</h1>
-      <Curiosidades/>
-      <h1 id='titulo'>Projetos</h1>
-      {projetos}
+      <Indice
+        handleClickResumo={mostrarResumo}
+        handleClickAcademico={mostrarAcademico}
+        handleClickExperiencia={mostrarExperiencia}
+        handleClickCuriosidade={mostrarCuriosidade}
+        handleClickProjeto={mostrarProjeto}
+      />
+      {resumo&& <h1 id='titulo'>Resumo</h1>}
+      {resumo&& <Resumo/>}
+      {academia && <h1 id='titulo'>Formação Acadêmica</h1>}
+      {academia && <Academico/>}
+      {experiencia && <h1 id='titulo'>Experiência Anterior</h1>}
+      {experiencia && <Experiencias/>}
+      {curiosidade && <h1 id='titulo'>Curiosidades</h1>}
+      {curiosidade && <Curiosidades/>}
+      {projeto && <h1 id='titulo'>Projetos</h1>}
+      {projeto && projetos}
     </>
   )
 }
--- porfolio/src/components/Indice.jsx
+++ porfolio/src/components/Indice.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-export default function Indice(){
+export default function Indice(props){
     return(
         <section className="center" id='indice'>
             <h1 id='titulo--indice'>Índice</h1>
             <ol>
                 <h2>
-                    <a href='#resumo'>
+                    <a onClick={props.handleClickResumo}>
                         <li>Resumo</li>
                     </a>
                 </h2>
                 <h2>
-                    <a href="#academico">
+                    <a onClick={props.handleClickAcademico}>
                         <li>Formação Acadêmica</li>
                     </a>
-                    <a href="#experiencia">
+                    <a onClick={props.handleClickExperiencia}>
                         <li>Experiência Anterior</li>
                     </a>
-                    <a href="#curiosidade">
+                    <a onClick={props.handleClickCuriosidade}>
                         <li>Curiosidades</li>
                     </a>
-                    <a href="#projeto">
+                    <a onClick={props.handleClickProjeto}>
                         <li>Projetos</li>
                     </a>
                 </h2>
